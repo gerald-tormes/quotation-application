0938ffccbf8f7bb0d0fc0634c95601ca3faaf878
Gerald <tormes.gerald@gmail.com>
2025-08-02 16:15:43 +0800

    feat(AuditLog): define fillable attributes, casts, belongsTo user, and morphTo model relationship

--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityLog extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     * @var array<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'model',
+        'model_id',
+        'action',
+        'data',
+        'old_values',
+    ];
+
+    /**
+     * Summary of casts
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+        'old_values' => 'array',
+    ];
+
+    /**
+     * Summary of user
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<User, ActivityLog>
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Summary of model
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo<Model, ActivityLog>
+     */
+    public function model()
+    {
+        return $this->morphTo();
+    }
 }

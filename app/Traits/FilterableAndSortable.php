ebefd06512b0c6d0f714dacf40108a6100bfb5b3
Gerald <tormes.gerald@gmail.com>
2025-08-02 16:20:42 +0800

    fix(trait): import missing Builder class

---
+++ app/Traits/FilterableAndSortable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait FilterableAndSortable
+{
+    /**
+     * Retrieve filterable and sortable items with pagination.
+     *
+     * @param array $filters
+     * @param array $sorting
+     * @param int $perPage
+     * @return LengthAwarePaginator
+     */
+    public function getFilterableAndSortable(array $filters, array $sorting, int $perPage): LengthAwarePaginator
+    {
+         $query = $this->buildQuery($filters);
+
+        return $query->orderBy($sorting['field'], $sorting['direction'])
+            ->paginate($perPage)
+            ->withQueryString()
+            ->appends([
+                'sortField' => $sorting['field'],
+                'sortDirection' => $sorting['direction'],
+            ]);
+    }
+
+    /**
+     * Build the query based on the provided filters.
+     *
+     * @param array $filters
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    abstract protected function buildQuery(array $filters): Builder;
+}

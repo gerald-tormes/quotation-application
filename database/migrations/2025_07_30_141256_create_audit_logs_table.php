27ecbedbd3b7bf0deb6321e3e99154108af16e9c
Gerald <tormes.gerald@gmail.com>
2025-08-02 16:21:47 +0800

    feat(activity-log): add migration for activity_logs table

---
+++ database/migrations/2025_07_30_141256_create_audit_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->string('model');
+            $table->unsignedBigInteger('model_id');
+            $table->string('action');
+            $table->json('data')->nullable();
+            $table->json('old_values')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audit_logs');
+    }
+};
